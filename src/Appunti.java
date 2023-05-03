a263c6470d7d424f163e72f46598a48d33cc54c8
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-05-03 10:50:12 +0200

    Update Appunti.java

--- src/Appunti.java
+++ src/Appunti.java
@@ -4,5 +4,54 @@ public class Appunti {
 	public static void main(String[] args) {
 System.out.println("APPUNTI GENERALI D2");
 	}
-
 }
+//ogni app avrà un suo metodo main 
+
+
+//public class Application {
+//
+//	public static void main(String[] args) {
+//
+//		// Creo istanze di Human
+//
+//		Human myFirstHuman = new Human("Aldo","Baglio");
+//		myFirstHuman.firstName = "Aldo";
+//		myFirstHuman.lastName = "Baglio";
+//		System.out.println(myFirstHuman.firstName + " " + myFirstHuman.lastName);
+//		myFirstHuman.sayHello();
+//
+//		Human mySecondHuman = new Human("Giovanni","Storti");
+//		mySecondHuman.firstName = "Giovanni";
+//		mySecondHuman.lastName = "Storti";
+//		System.out.println(mySecondHuman.firstName + " " + mySecondHuman.lastName);
+//		mySecondHuman.sayHello();
+//
+//
+//	}
+//}
+//
+//class Human {
+//	// Attributi
+//	String firstName;
+//	String lastName;
+
+//Costruttori (deve chiamarsi come la classe)
+//public Human(){   //costruttore1
+//this.firstName = "Giacomo";
+//this.secondName = "Poretti";
+
+//public Human(String fn){   //costruttore2
+//this.firstName = fn;
+//this.secondName = "Cognome generico";
+//
+//public Human(String fn,String ln){ //costruttore3
+//this.firstName = fn; 
+//this.secondName = ln;}
+//}
+
+
+//	// Metodi
+//	void sayHello() {
+//		System.out.println("Ciao!"+this.firstName);
+//	}
+//}
